4f4987be657587968cc36500fd587127833839dc
jonathan strid <pjstrid@outlook.com>
2025-09-25 12:43:06 +0100

    added comments

--- src/ZooApp.java
+++ src/ZooApp.java
@@ -9,28 +9,31 @@ public class ZooApp {
     public ZooApp(){
         initList();
         System.out.println("To start, press 'ENTER'");
-        InputHandler.getString();
+        InputHandler.getString(); // Denna rad gör så det krävs ett enter-tryck för att fortsätta
         presentAnimals();
-        InputHandler.getString();
+        InputHandler.getString(); // Samma här
         featuresOfAnimals();
-        InputHandler.getString();
+        InputHandler.getString(); // Samma här
         currentActivity();
 
     }
 
 
-
+// Metod som gör så att djuren presenterar sig
     public static void presentAnimals() {
         System.out.println("All of the animals now presents themself:");
         System.out.println();
         for (Animal animal : animals) {
-            System.out.println(animal.name + ": " + "'I am " + animal.name + " the " + animal.getClass().getName() + "'");
+            System.out.println(animal.name + ": " + "'I am " +             // Hämtar namnet på djuret från ArrayList
+                                 animal.name + " the " +
+                                    animal.getClass().getName() + "'");    // Hämtar vilken klass djuret tillhör
         }
         System.out.println("-------------------------");
         System.out.println("To continue, press 'ENTER'");
-
     }
 
+
+// Metod som visar alla attribut för varje djur
     public static void featuresOfAnimals(){
         System.out.println("This is what all of the animals can do:");
 
@@ -39,44 +42,48 @@ public class ZooApp {
             animal.eat();
             animal.makeSound();
 
-            if (animal instanceof Swimmable s){
-                s.swim();
+            if (animal instanceof Swimmable s){     // Hämtar info från interface,
+                s.swim();                           // om djuret kan simma skrivs det ut,
+                                                    // annars ingen utskrift här på det djuret
             }
-            if (animal instanceof Flyable f){
-                f.fly();
+            if (animal instanceof Flyable f){       // Hämtar info från interface,
+                f.fly();                            // om djuret kan flyga skrivs det ut,
+                                                    // annars ingen utskrift här på det djuret
             }
         }
         System.out.println("-------------------------");
         System.out.println("To continue, press 'ENTER'");
     }
 
+
+// Metod som slumpar fram en aktuell aktivitet för varje djur
     public static void currentActivity(){
         System.out.println("This is what the animals do right now:");
         System.out.println();
         for (Animal animal : animals){
             Random random = new Random();
-            int randomNumber = random.nextInt(1, 5);
+            int randomNumber = random.nextInt(1, 5);    // upprepad slumpvis siffra mellan 1 och 4 för varje djur
 
             if (randomNumber == 1) {
-
                 animal.eat();
-            } else if (randomNumber == 2) {
 
+            } else if (randomNumber == 2) {
                 animal.makeSound();
+
             } else if (randomNumber == 3) {
 
-                if (animal instanceof Swimmable s){
+                if (animal instanceof Swimmable s){ // om den kan simma, skrivs det ut
                     s.swim();
-                } else{
+                } else{                            // om djuret inte har ett tredje attribut, kommer denne göra sitt läte
                     animal.makeSound();
                     System.out.print("because he can't swim");
                     System.out.println();
                 }
             } else if (randomNumber == 4){
 
-                if (animal instanceof Flyable f){
+                if (animal instanceof Flyable f){   // om den kan flyga, skrivs det ut
                     f.fly();
-                } else {
+                } else {                           // om djuret inte har ett fjärde attribut, kommer denne äta sin mat
                     animal.eat();
                     System.out.print("because he can't fly");
                     System.out.println();
@@ -87,6 +94,8 @@ public class ZooApp {
         System.out.println("To quit, press 'ENTER'");
     }
 
+
+// Skapar lista med fyra djur
     public static void initList(){
         animals.add(new Dolphin("Dave"));
         animals.add(new Owl("Owen"));
